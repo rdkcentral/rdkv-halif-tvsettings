c42617d3d23ae89e4b2575ec1381f5c7c5ab75b9
Rajaselvam, ThanuShree (Software Engineer - Sky - Guest) <thanushree.rajaselvam@sky.uk>
2023-04-04 13:09:48 +0530

    added missing comment in tvLog.h
    Signed-off-by: Rajaselvam, ThanuShree (Software Engineer - Sky - Guest) <thanushree.rajaselvam@sky.uk>

--- include/tvLog.h
+++ include/tvLog.h
@@ -1,3 +1,30 @@
+/*
+* If not stated otherwise in this file or this component's Licenses.txt file the
+* following copyright and licenses apply:
+*
+* Copyright 2016 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/**
+* @addtogroup HPK
+* @{
+**/
+/**
+* @addtogroup tvsettings
+* @{
+**/
+
 #ifndef _TV_LOG_H
 #define _TV_LOG_H
 
@@ -9,20 +36,21 @@ extern "C"
 {
 #endif
 
-
+/* Macro defined to log ERROR log messages */
 #define ERROR(text, ...) do {\
         printf("%s[%d] - %s: " text, __FUNCTION__, __LINE__, "ERROR", ##__VA_ARGS__);}while(0);
 
+/* Macro defined to log INFO log messages */
 #define INFO(text, ...) do {\
         printf("%s[%d] - %s: " text, __FUNCTION__, __LINE__, "INFO", ##__VA_ARGS__);}while(0);
 
-//#define DEBUG_ENABLED
+/* Macro defined to log DEBUG log messages */
 #ifdef DEBUG_ENABLED
 
 #define DEBUG(text, ...) do {\
         printf("%s[%d] - %s: " text, __FUNCTION__, __LINE__, "DEBUG", ##__VA_ARGS__);}while(0);
 #else
-#define DEBUG(text, ...) 
+#define DEBUG(text, ...)
 #endif
 
 
@@ -30,4 +58,7 @@ extern "C"
 }
 #endif
 
-#endif //_TV_LOG_H
+#endif // _TV_LOG_H
+
+/** @} */ // End of tvsettings
+/** @} */ // End of HPK
