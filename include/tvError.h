4ff23dd8b09bed9a30419535d8222b9a859d68b0
Rajaselvam, ThanuShree (Software Engineer - Sky - Guest) <thanushree.rajaselvam@sky.uk>
2023-04-04 13:05:43 +0530

    added missing comment in tvError.h
    Signed-off-by: Rajaselvam, ThanuShree (Software Engineer - Sky - Guest) <thanushree.rajaselvam@sky.uk>

--- include/tvError.h
+++ include/tvError.h
@@ -1,3 +1,30 @@
+/*
+* If not stated otherwise in this file or this component's Licenses.txt file the
+* following copyright and licenses apply:
+*
+* Copyright 2016 RDK Management
+*
+* Licensedunder the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/**
+* @addtogroup HPK
+* @{
+**/
+/**
+* @addtogroup tvsettings
+* @{
+**/
+
 #ifndef _TV_ERROR_H
 #define _TV_ERROR_H
 
@@ -6,18 +33,21 @@ extern "C"
 {
 #endif
  
-//! The enumeration of Error codes 
+/* Error codes defined for tvError_t */
 typedef enum {
      
-    tvERROR_NONE = 0, //! Error Code None
-    tvERROR_GENERAL, //! Error Code General  
-    tvERROR_OPERATION_NOT_SUPPORTED,  //! Error code Operation not supported 
-    tvERROR_INVALID_PARAM,  //! Error code - Invalid Parameter  
-    tvERROR_INVALID_STATE  //! Error code - Invalid State
+    tvERROR_NONE = 0,                   //!< No error
+    tvERROR_GENERAL,                    //!< Other errors from drivers
+    tvERROR_OPERATION_NOT_SUPPORTED,    //!< Operation not supported
+    tvERROR_INVALID_PARAM,              //!< Invalid Parameter error
+    tvERROR_INVALID_STATE               //!< Invalid State error
 }tvError_t;
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //_TV_ERROR_H
+#endif // _TV_ERROR_H
+
+/** @} */ // End of tvsettings
+/** @} */ // End of HPK
