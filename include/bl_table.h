7110e5ce7b33b6a3b079b99363aa7f1947f6a6a9
Rajaselvam, ThanuShree (Software Engineer - Sky - Guest) <thanushree.rajaselvam@sky.uk>
2023-04-04 12:49:10 +0530

    added missing comment in bl_table.h
    Signed-off-by: Rajaselvam, ThanuShree (Software Engineer - Sky - Guest) <thanushree.rajaselvam@sky.uk>

--- include/bl_table.h
+++ include/bl_table.h
@@ -1,10 +1,40 @@
+/*
+* If not stated otherwise in this file or this component's Licenses.txt file the
+* following copyright and licenses apply:
+*
+* Copyright 2016 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/**
+* @addtogroup HPK
+* @{
+**/
+/**
+* @addtogroup tvsettings
+* @{
+**/
+
 #ifndef __BL_TABLE_INFO_H
 #define __BL_TABLE_INFO_H
 
-
+/* File that containes the blacklight values for panel */
 #define BACKLIGHT_FILE_NAME  "backlight_values.ini" 
  
-const int defaultSDR = 50; //!< default SDR values
-const int defaultHDR = 100; //!< default HDR values
+const int defaultSDR = 50;       //!< default SDR value
+const int defaultHDR = 100;      //!< default HDR value
 
-#endif // __BL_TABLE_INFO_H 
+#endif // __BL_TABLE_INFO_H
+
+/** @} */ // End of tvsettings
+/** @} */ // End of HPK
