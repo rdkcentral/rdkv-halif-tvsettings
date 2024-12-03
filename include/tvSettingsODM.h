4274dcdcc48b67c8e153c34079f2806a2eeec08d
arjunbinu <51983664+arjunbinu@users.noreply.github.com>
2024-12-03 17:28:31 +0000

    gh #55 Remove ODM specific apis - phase2

--- include/tvSettingsODM.h
+++ include/tvSettingsODM.h
@@ -83,100 +83,26 @@ void SetGammaTestPoint( int length, unsigned short gamma[ ]);
 void GetGammaTestPoint( int *length, unsigned short gamma[]);
 
 /**
- * @brief Gets the supported HLG modes and their count. To be deprecated soon.
+ * @brief Sets the white balance control. To be deprecated soon.
  * @note SOC vendors can stub these ODM functions and mark it as weak
  *
- * This function returns the supported HLG modes and their count
+ * This function sets the white balance for a specific source, color, color temperature and control.
  *
- * @param[out] dvModes[]            - List of available HLG modes. Refer ::pic_modes_t and ::tvDolbyMode_t @n
- *                                    Valid values are "dark" and & "bright" for ::pic_modes_t.name @n
- *                                    Valid values are member of ::tvDolbyMode_t tvHLGMode_Dark and ::tvDolbyMode_t tvHLGMode_Bright @n
- *                                    for ::pic_modes_t.value.
- * @param[out] count                - Count of supported HLG modes. Max and min is 2
+ * @param[in] inputSrc                    - Source of input to be set. Valid values are ( "HDMI", "TV", "AV" )
+ * @param[in] colorTemp                   - Color temperature value to be set. Valid values are ( "custom", "normal", "cool", "warm" )
+ * @param[in] color                       - Color to be set. Valid values are ( "red", "blue", "green" )
+ * @param[in] ctrl                        - Control to be set. Valid values are ( "gain", "offset" )
+ * @param[in] value                       - Values to be set for the specific control. Valid range : for gain is ( 0 to 2047 ), for offset is ( -1024 to 1024 )
  *
  * @return tvError_t
  *
- * @retval tvERROR_NONE                      - Success
- * @retval tvERROR_INVALID_PARAM             - Input parameter is invalid
- * @retval tvERROR_OPERATION_NOT_SUPPORTED   - Operation is not supported
- * @retval tvERROR_GENERAL                   - Underlying failures - SoC, memory, etc
- * 
- * @pre tvInit() should be called before calling this API
- */
-tvError_t GetTVSupportedHLGModesODM(pic_modes_t *dvModes[],unsigned short *count);
-
-/**
- * @brief Sets the HLG mode. To be deprecated soon.
- * @note SOC vendors can stub these ODM functions and mark it as weak
- *
- * This function sets the HLG mode in Dolby vision core.
- * The change takes effect for current primary video source selected, primary video format if handled via Dolby vision core 
- * and picture mode selected, but not saved for future use. The value is discarded if primary video format or 
- * primary video source or picture mode changes.
- *
- * @param[in] hlgMode              - HLG mode to be set. Valid values can be one of the 
- *                                     strings from ::pic_modes_t.name parameter returned by GetTVSupportedHLGModesODM
- *
- *
- * @return tvError_t
- *
- * @retval tvERROR_NONE                      - Success
- *
- * @retval tvERROR_INVALID_PARAM             - Input parameter is invalid
- * @retval tvERROR_INVALID_STATE             - Interface is not initialized
- * @retval tvERROR_OPERATION_NOT_SUPPORTED   - Operation is not supported
- * @retval tvERROR_GENERAL                   - Underlying failures - SoC, memory, etc
+ * @retval tvERROR_NONE                   - Success
+ * @retval tvERROR_INVALID_PARAM          - Input parameter is invalid
+ * @retval tvERROR_INVALID_STATE          - When the input value is not valid
+ * @retval tvERROR_GENERAL                - Underlying failures - SoC, memory, etc
  *
  * @pre tvInit() should be called before calling this API
  */
-tvError_t SetTVHLGModeODM(const char * hlgMode);
-
-/**
- * @brief Gets the supported HDR10 modes and their count. To be deprecated soon.
- * @note SOC vendors can stub these ODM functions and mark it as weak
- *
- * This function returns the supported HDR10 modes and their count
- *
- * @param[out] dvModes[]            - List of available HDR10 modes. Refer ::pic_modes_t and ::tvDolbyMode_t @n
- *                                    Valid values are "dark" and & "bright" for ::pic_modes_t.name @n
- *                                    Valid values are member of ::tvDolbyMode_t tvHDR10Mode_Dark and ::tvDolbyMode_t tvHDR10Mode_Bright @n
- *                                    for ::pic_modes_t.value.
- * @param[out] count                - Count of supported HDR10 modes. Max and min is 2
- *
- * @return tvError_t
- *
- * @retval tvERROR_NONE                      - Success
- * @retval tvERROR_INVALID_PARAM             - Input parameter is invalid
- * @retval tvERROR_OPERATION_NOT_SUPPORTED   - Operation is not supported
- * @retval tvERROR_GENERAL                   - Underlying failures - SoC, memory, etc
- *
- * @pre tvInit() should be called before calling this API
- */
-tvError_t GetTVSupportedHDR10ModesODM(pic_modes_t *dvModes[],unsigned short *count);
-
-/**
- * @brief Sets the HDR10 mode. To be deprecated soon.
- * @note SOC vendors can stub these ODM functions and mark it as weak
- *
- * This function sets the HDR10 mode in Dolby vision core.
- * The change takes effect for current primary video source selected, primary video format if handled via Dolby vision core 
- * and picture mode selected, but not saved for future use. The value is discarded if primary video format or 
- * primary video source or picture mode changes.
- *
- * @param[in] hdr10Mode              - HDR10 mode to be set. Valid values can be one of the 
- *                                     strings from ::pic_modes_t.name parameter returned by GetTVSupportedHDR10ModesODM
- *
- * @return tvError_t
- *
- * @retval tvERROR_NONE                      - Success
- * @retval tvERROR_INVALID_PARAM             - Input parameter is invalid
- * @retval tvERROR_INVALID_STATE             - Interface is not initialized
- * @retval tvERROR_OPERATION_NOT_SUPPORTED   - Operation is not supported
- * @retval tvERROR_GENERAL                   - Underlying failures - SoC, memory, etc
- *
- * @pre tvInit() should be called before calling this API
- */
-tvError_t SetTVHDR10ModeODM(const char * hdr10Mode);
 
 #ifdef __cplusplus
 }
